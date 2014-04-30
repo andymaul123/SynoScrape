78677abbd423d0a34b7d19a6606de2d39979ff09
Andrew Maul <andrew.maul@blueearth.net>
2014-04-30 15:10:20 -0500

    Client-side scraping working using Yahoo's YQL in a local html page.

--- SynoScrape/contextLaunch.js
+++ SynoScrape/contextLaunch.js
@@ -1,5 +1,32 @@
-function genericOnClick(info, tab) {
-    console.log("yay!");
-}
+/*
+chrome.contextMenus.create({
+    "title": "Synonyms", 
+    "contexts":["selection"],
+    "onclick": genericOnClick
+});
+var selectedText;
 
-var testVar = chrome.contextMenus.create({"title": "Synonyms", "contexts":["selection"],"onclick": genericOnClick});
+function genericOnClick(onClickData) {
+    selectedText = onClickData.selectionText;
+    console.log(selectedText);
+//begin ajax request
+    $('#container').load('http://google.com'); // SERIOUSLY!
+ 
+    $.ajax({
+        url: 'http://news.bbc.co.uk',
+        type: 'GET',
+        success: function(res) {
+            var headline = $(res.responseText).find('a.tsh').text();
+            alert(headline);
+        }
+    });
+ 
+};
+*/
+
+$(document).ready(function() {
+    requestCrossDomain('http://www.cnn.com', function(results) {
+       $('#container').html(results);
+});
+
+})
