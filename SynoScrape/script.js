70690bbca09c1c97f0d9b038f97c78dba302e0f9
Andrew Maul <andrew.maul@blueearth.net>
2014-05-06 11:54:50 -0500

    Decided to drop the idea of displaying in context menu due to timing issues. Now just using OnClick in the menu to create a window alert.

--- SynoScrape/script.js
+++
@@ -1,9 +0,0 @@
-/* Listens for when a user right clicks and logs their selection to local storage. */
-window.onmouseup = function() {
-    var capturedSelection = window.getSelection().toString();
-    if(capturedSelection.length >= 1) {
-        chrome.storage.local.set({'userSelection': capturedSelection});
-        console.log("Script.js ran successfully.");
-    }
-};
-
---
+++ SynoScrape/synoscrape.js
@@ -0,0 +1,44 @@
+var selectedText;
+var trimmedText;
+var cleanedText;
+var queryText;
+var stringContainsSpaces;
+var items = [];
+var synonyms;
+
+var createMenu = chrome.contextMenus.create({
+    "title": "Scrape Synonyms",
+    "contexts":["selection"],
+    "onclick": genericOnClick
+});
+
+function genericOnClick(onClickData) {
+    selectedText = onClickData.selectionText;
+    trimmedText = selectedText.replace(/[\.,\/#!$%\^&\*;:{}=_`~()]/g,"");
+    $.trim(trimmedText);
+    hasWhiteSpace(trimmedText);
+    function hasWhiteSpace(s) {
+        stringContainsSpaces = trimmedText.indexOf(' ') >= 0;
+        if(stringContainsSpaces) {
+            //console.log("Spaces detected. Cleaning...");
+            cleanedText = trimmedText.match(/^(\S+)\s(.*)/).slice(1);
+            queryText = cleanedText[0];
+            //console.log(queryText);
+        }
+        else {
+            //console.log("no spaces");
+            queryText = trimmedText;
+            //console.log(queryText);
+        }
+    }
+    $.get("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22www.thesaurus.com%2Fbrowse%2F" + queryText + "%22", function(data){useReturnData(data);}, 'xml');
+    function useReturnData(data){
+        for(i=0; i<=4; i++) {
+            foundItem = $(data).find('#synonyms-0 .relevancy-list ul:first li:nth-child(' + (i+1) +') a span.text').text();
+            items.push(foundItem);
+            //console.log(foundItem);
+            synonyms = items.join('\n');
+        }
+        window.alert("Synonyms for " + queryText + " are :" + "\n" + "\n" + synonyms);
+    };
+}
